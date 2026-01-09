3d5857b24669af1a314152fb47dd5f69703b5693
TeddouX <victor.beasse6@gmail.com>
2026-01-09 23:31:15 +0100

    Fixed material_manager.hpp being in src/

--- noctis_engine/include/noctis_engine/utils/sparse_set.hpp
+++ noctis_engine/include/noctis_engine/utils/sparse_set.hpp
@@ -1,8 +1,17 @@
 #pragma once
+#include <vector>
 
 namespace NoctisEngine
 {
     
+template <typename KeyType_, typename MappedType_>
+class SparseSet {
+public:
 
-    
+
+private:
+    std::vector<std::size_t> dense_;
+    std::vector<MappedType_> sparse_;
+};
+
 } // namespace NoctisEngine
